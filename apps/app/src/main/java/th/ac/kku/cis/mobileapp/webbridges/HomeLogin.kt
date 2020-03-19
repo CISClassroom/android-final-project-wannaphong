813443cd67add9d60a544c89c8f5ec12939202c8
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-20 02:37:40 +0700

    Add checkSelfPermission

--- apps/app/src/main/java/th/ac/kku/cis/mobileapp/webbridges/HomeLogin.kt
+++ apps/app/src/main/java/th/ac/kku/cis/mobileapp/webbridges/HomeLogin.kt
@@ -1,5 +1,7 @@
 package th.ac.kku.cis.mobileapp.webbridges
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,20 @@ class HomeLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_login)
+        if(checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED ){
+            val permission = arrayOf(Manifest.permission.INTERNET)
+            requestPermissions(permission,1)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when(requestCode){
+            1 -> {
+            }
+        }
     }
 }
