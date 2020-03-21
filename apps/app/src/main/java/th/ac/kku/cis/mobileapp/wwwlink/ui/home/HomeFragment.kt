55695d5440d305d5e873f9aba5ffdc1f8f3a4605
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-22 01:38:23 +0700

    Update HomeFragment.kt

--- apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/home/HomeFragment.kt
+++ apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/home/HomeFragment.kt
@@ -14,8 +14,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import org.jsoup.Jsoup
 import th.ac.kku.cis.mobileapp.wwwlink.R
 import th.ac.kku.cis.mobileapp.wwwlink.URLItem
@@ -73,17 +72,28 @@ class HomeFragment : Fragment() {
         dialog.setPositiveButton("Submit"){
                 dialog,positiveButton ->
             var uid:String = auth.currentUser!!.uid
-            var findurl = mDB.child("URL_item")
             var newURL = URLItem.create()
             var url:String = et.text.toString()
             newURL.url = url
             Log.w("URL",newURL.url)
-            var isok=findurl.orderByChild("url").equalTo(url).ref
+            var key:String? = ""
+            var isok=mDB.child("URL_item").orderByChild("url").equalTo(url).limitToFirst(1).addValueEventListener(object:ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
 
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (ds in dataSnapshot.children) {
+                        key = ds.child("objID").getValue(String::class.java)
+
+                    }
+                }
+            })
             val newURL2user:UserLink = UserLink.create()
+            Log.w("Key Key",key)
 
             var k:String? =null
-            if(isok.key.toString()=="URL_item") {
+            if(key.toString()=="") {
             try{
                 var doc = Jsoup.connect(url).get()
                 //newURL.URLtitle = doc.title().toString()
@@ -99,7 +109,7 @@ class HomeFragment : Fragment() {
             k = newItemDB.key
             }
             else{
-                k = isok.key.toString()
+                k = key.toString()
             }
             newURL2user.URLobj=k
             val newItemDB2 = mDB.child("URL").child(uid).push()
