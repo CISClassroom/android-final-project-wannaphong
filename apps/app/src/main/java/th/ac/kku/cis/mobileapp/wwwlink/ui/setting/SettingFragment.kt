668da694474dd00869254fd81dec2aa9bdd5f873
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-21 17:54:20 +0700

    fetch name and image profile

--- apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/setting/SettingFragment.kt
+++ apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/setting/SettingFragment.kt
@@ -1,12 +1,17 @@
 package th.ac.kku.cis.mobileapp.wwwlink.ui.setting
 
+import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 
 import th.ac.kku.cis.mobileapp.wwwlink.R
 
@@ -17,6 +22,7 @@ class SettingFragment : Fragment() {
     }
 
     private lateinit var viewModel: SettingViewModel
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +30,11 @@ class SettingFragment : Fragment() {
     ): View? {
         val root= inflater.inflate(R.layout.setting_fragment, container, false)
         val NameSetting: TextView = root.findViewById(R.id.NameSetting)
-        NameSetting.text = "แมว"
+        val Profile:ImageView = root.findViewById(R.id.imageProfile)
+        auth = FirebaseAuth.getInstance()
+        val xx: Uri? = auth.currentUser!!.photoUrl
+        NameSetting.text = auth.currentUser!!.displayName.toString()
+        Picasso.get().load(xx).into(Profile)
         return root
     }
 
