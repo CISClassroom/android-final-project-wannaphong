4f90d81eb9ca9090f4ab99c77bff715d70c91855
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-21 18:28:32 +0700

    Add logout from setting

--- apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/setting/SettingFragment.kt
+++ apps/app/src/main/java/th/ac/kku/cis/mobileapp/wwwlink/ui/setting/SettingFragment.kt
@@ -7,11 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.setting_fragment.*
+import th.ac.kku.cis.mobileapp.wwwlink.MainActivity
 
 import th.ac.kku.cis.mobileapp.wwwlink.R
 
@@ -34,8 +39,16 @@ class SettingFragment : Fragment() {
         auth = FirebaseAuth.getInstance()
         val xx: Uri? = auth.currentUser!!.photoUrl
         NameSetting.text = auth.currentUser!!.displayName.toString()
+        val LogoutBtn:Button = root.findViewById(R.id.logoutSetting)
+        LogoutBtn.setOnClickListener {
+            singOut()
+        }
         Picasso.get().load(xx).into(Profile)
         return root
+    }
+    private fun singOut() {
+        auth.signOut()
+        this.getActivity()?.finish();
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
